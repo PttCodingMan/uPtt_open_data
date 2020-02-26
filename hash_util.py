6313119d44403738b16f7ceea67493f1cd10a812
CodingMan <CodingMan@uptt.cc>
2020-02-26 16:24:55 +0800

    add test

--- hash_util.py
+++ hash_util.py
@@ -7,4 +7,10 @@ def sha256(s):
     hash = hashlib.sha256(s_lower.encode('utf-8')).hexdigest()
     print(f'[{s}]\n[{s_lower}]\n[{hash}]')
 
-sha256('QQ_id')
+# sha256('QQ_id')
+
+# test 
+import urllib.request, json
+with urllib.request.urlopen("https://raw.githubusercontent.com/PttCodingMan/uPtt_open_data/master/list/blacklist.json") as url:
+    data = json.loads(url.read().decode())
+    print(data)
